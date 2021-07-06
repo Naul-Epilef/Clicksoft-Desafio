aaa6e42ddb2e87a278266fbf8f61830e9ab30d1b
Luan-Felipe <lfps1999@gmail.com>
2021-07-06 16:19:13 -0300

    fix: refactoring the routes to use react-navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,60 @@
 import React from 'react';
 
-import {Router, Scene} from 'react-native-router-flux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Home from './home';
-import User from './user';
-import Post from './post';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Post from '../screens/Post/List';
+import PostCreate from '../screens/Post/Form';
+import User from '../screens/User';
 
 const Routes = () => {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <Router>
-      <Scene key="pri" tabs={true} tabBarPosition="bottom">
-        <Scene key="Home" title="Inicio" component={Home} initial={true} />
-        <Scene key="sec" tabs={true} tabBarPosition="bottom" back={true}>
-          <Scene
-            key="User"
-            title="Usuário"
-            component={User}
-            back={true}
-            hideTabBar={true}
-          />
-          <Scene
-            key="Post"
-            title="Post"
-            component={Post}
-            back={true}
-            hideTabBar={true}
-          />
-        </Scene>
-      </Scene>
-    </Router>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Post"
+        tabBarOptions={{
+          activeTintColor: '#2296f3',
+          inactiveTintColor: 'black',
+          style: {
+            height: 55,
+          },
+          labelStyle: {
+            fontSize: 14,
+          },
+        }}>
+        <Tab.Screen
+          name="Post"
+          component={Post}
+          options={route => ({
+            tabBarLabel: 'Feed',
+            tabBarIcon: ({focused, color, size}) => {
+              const name = focused ? 'home' : 'home-outline';
+              return <Ionicons name={name} size={35} color={color} />;
+            },
+          })}
+        />
+        <Tab.Screen
+          name="PostCreate"
+          component={PostCreate}
+          options={route => ({
+            tabBarLabel: 'Criar post',
+            tabBarIcon: ({focused, color, size}) => {
+              const name = focused ? 'add-circle' : 'add-circle-outline';
+              return <Ionicons name={name} size={35} color={color} />;
+            },
+          })}
+        />
+        <Tab.Screen
+          name="User"
+          component={User}
+          options={{tabBarLabel: 'Usuário', tabBarButton: () => null}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
